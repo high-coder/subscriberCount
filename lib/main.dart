9c4f16683aac2a84ecd58533c3d5c0e5d989911d
high-coder <teentechreviewer15@gmail.com>
2023-04-13 20:29:32 +0530

    part 2

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:lottie/lottie.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String apiKey = "AIzaSyDSVDavZvVtU3CF2Rr7KT7ZA_sYn4Ky64I";
+  //String apiKey = "AIzaSyBbjIyOH0GsFy9NoyOdeiydpN8M4ZfLs9s";
+  String apiKey = "AIzaSyAN2LMreWFxYO6XtlTKASmbk1dRe6LOOXI";
   String channelId = "UC4YI8YjlbPwNfQgsPysG-IA";
   getResponse() async {
     print("Entering here mate");
@@ -38,6 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
         "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=$channelId&key=$apiKey";
     var resp = await http.get(Uri.parse(apiEndPoint));
     var decodeResponse = jsonDecode(resp.body);
+    print(decodeResponse);
     String newCount =
         decodeResponse['items'][0]['statistics']['subscriberCount'].toString();
     if (newCount != subCount) {
@@ -51,15 +54,91 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   getChannelImage() async {
-    String endPoint2 =
-        "https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items%2Fsnippet%2Fthumbnails%2Fdefault&id=$channelId&key=$apiKey";
-    var resp = await http.get(Uri.parse(endPoint2));
-    var decodeResponse = jsonDecode(resp.body);
-    channelImage =
-        decodeResponse["items"][0]["snippet"]["thumbnails"]["default"]["url"];
-    setState(() {});
-    print(channelImage);
-    print(decodeResponse);
+    try {
+      String endPoint2 =
+          "https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items%2Fsnippet%2Fthumbnails%2Fdefault&id=$channelId&key=$apiKey";
+      var resp = await http.get(Uri.parse(endPoint2));
+      var decodeResponse = jsonDecode(resp.body);
+      print(decodeResponse);
+      channelImage =
+          decodeResponse["items"][0]["snippet"]["thumbnails"]["default"]["url"];
+      setState(() {});
+      print(channelImage);
+      print(decodeResponse);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  int totalViews = 22000;
+  void fetchChannelData() async {
+    //totalViews = 0;
+    String? nextPageToken;
+
+    // Loop through pages of video data for the channel
+    do {
+      // Fetch video data for the channel with pageToken
+      String? apiUrl;
+      if (nextPageToken != null) {
+        apiUrl =
+            'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=$channelId&type=video&key=$apiKey&pageToken=$nextPageToken';
+      } else {
+        apiUrl =
+            'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=$channelId&type=video&key=$apiKey';
+        // apiUrl =
+        //     'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=$channelId&type=video&key=$apiKey';
+      }
+      DateTime currentDate = DateTime.now();
+      DateTime ninetyDaysAgo = currentDate.subtract(Duration(days: 90));
+      var response = await http.get(Uri.parse(apiUrl));
+      //print(response.body);
+      if (response.statusCode == 200) {
+        // Parse the response JSON
+        var data = jsonDecode(response.body);
+        List<dynamic> items = data['items'];
+
+        // Loop through each video and fetch view counts
+        for (var item in items) {
+          print("this is the item $item");
+          String videoId = item['id']['videoId'];
+          String date = item["snippet"]["publishedAt"];
+          DateTime publishDate = DateTime.parse(date).toLocal();
+          if (publishDate.isAfter(ninetyDaysAgo) &&
+              publishDate.isBefore(currentDate)) {
+            int viewCount = await fetchVideoViewCount(videoId);
+            totalViews += viewCount;
+            print("The date is within the last 90 days.");
+          } else {
+            print("The date is not within the last 90 days.");
+          }
+        }
+
+        nextPageToken = data['nextPageToken'];
+      } else {
+        print(
+            'Failed to fetch channel data. Status code: ${response.statusCode}');
+        nextPageToken = null;
+      }
+    } while (nextPageToken != null);
+
+    print('Total views in the last 90 days: $totalViews');
+  }
+
+  Future<int> fetchVideoViewCount(String videoId) async {
+    String apiUrl =
+        'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=$videoId&key=$apiKey';
+    var response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      int viewCount = int.parse(data['items'][0]['statistics']['viewCount']);
+      return viewCount;
+    } else {
+      print(response.body);
+      print(
+          'Failed to fetch video view count. Status code: ${response.statusCode}');
+      return 0;
+    }
   }
 
   String? channelImage;
@@ -69,22 +148,24 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     getChannelImage();
     getResponse();
+    fetchChannelData();
     // TODO: implement initState
     super.initState();
-    _timer = Timer.periodic(Duration(seconds: 60), (timer) async {
-      //print("this is that $timer");
-      await getResponse();
-      // if (temp != subCount)
-      //   setState(() {
-      //     subCount = temp;
-      //   });
-      // else {
-      //   // do nothing
-      //   print("no change in the subs");
-      // }
-    });
+    // _timer = Timer.periodic(Duration(seconds: 60), (timer) async {
+    //   //print("this is that $timer");
+    //   await getResponse();
+    //   // if (temp != subCount)
+    //   //   setState(() {
+    //   //     subCount = temp;
+    //   //   });
+    //   // else {
+    //   //   // do nothing
+    //   //   print("no change in the subs");
+    //   // }
+    // });
   }
 
+  /// Creating the ui for the same
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -145,10 +226,26 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Container(),
             Positioned(
-              bottom: 30,
+              bottom: 20,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  CircularPercentIndicator(
+                    radius: 120.0,
+                    lineWidth: 13.0,
+                    animation: true,
+                    percent: totalViews / 10000000,
+                    center: Text(
+                      "${(totalViews / 10000000) * 100}",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.white),
+                    ),
+                    // footer: ,
+                    circularStrokeCap: CircularStrokeCap.square,
+                    progressColor: Colors.red,
+                  ),
                   Container(
                     width: size.width,
                   ),
@@ -177,6 +274,20 @@ class _MyHomePageState extends State<MyHomePage> {
                   //         "https://assets7.lottiefiles.com/packages/lf20_qe6rfoqh.json"),
                   //   ),
                   // ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    " ${10000000 - totalViews} views to Monetize",
+                    style: GoogleFonts.openSans(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.0),
+                  ),
+
+                  SizedBox(
+                    height: 80,
+                  ),
                   Text("$subCount",
                       style: GoogleFonts.openSans(
                           color: Colors.white,
